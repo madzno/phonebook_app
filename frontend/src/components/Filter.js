1ea6fe9393885882b5801a726a973a3a54939c1d
Madz No <madzno12@gmail.com>
2023-07-17 11:39:21 -0400

    updating backend

--- frontend/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ currentName, handler }) => {
-  return (
-    <div>
-      filter shown with <input value={currentName} onChange={handler} />
-    </div>
-  )
-}
-
-export default Filter
