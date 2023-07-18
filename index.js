0a093e21e83e9e0a81a522e50b6580b81552dfa6
Madz No <madzno12@gmail.com>
2023-07-18 13:36:11 -0400

    adding error handling

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan('tiny'));
 
-let persons = [];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -42,11 +54,11 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
-    }).catch(error => console.log(error))
+    }).catch(error => next(error))
 })
 
 // let persons = [
@@ -72,10 +84,6 @@ app.delete('/api/persons/:id', (request, response) => {
 //   }
 // ];
 
-// app.get('/api/persons', (request, response) => {
-//   response.json(persons)
-// })
-
 // app.get('/info', (request, response) => {
 //   let numberOfPersons = persons.length;
 //   let currentTime = new Date();
@@ -94,47 +102,13 @@ app.delete('/api/persons/:id', (request, response) => {
 //   }
 // })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//   let id = Number(request.params.id);
-//   persons = persons.filter(person => person.id !== id);
-
-//   response.status(204).end();
-// })
-
-// const generateId = () => {
-//   let maxId = persons.length > 0
-//     ? Math.max(...persons.map(person => person.id))
-//     : 0
-//   return maxId + 1;
-// }
-
 // const nameNotUnique = (providedName) => {
 //   let names = persons.map(person => person.name);
 //   return names.includes(providedName);
 // }
 
-// app.post('/api/persons', (request, response) => {
-//   let body = request.body;
-
-//   if (!body.name) {
-//     return response.status(400).json({ error: 'name missing' });
-//   } else if (!body.number) {
-//     return response.status(400).json({ error: 'number missing' });
-//   } else if (nameNotUnique(body.name)) {
-//     return response.status(400).json({ error: 'must use unique name' });
-//   }
-
-//   let newPerson = {
-//     id: generateId(),
-//     name: body.name,
-//     number: body.number,
-//   }
-
-//   persons = persons.concat(newPerson);
-
-//   response.json(newPerson);
-
-// })
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
